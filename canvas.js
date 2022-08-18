59adf1015e33b4f8898bc7ac2f520fa15a467066
Nick <nickdem559@inbox.ru>
2022-08-18 18:05:23 +0300

    circles

--- canvas.js
+++ canvas.js
@@ -1,5 +1,12 @@
 import { buttons } from './content.js'
 
+
+let opt = {
+    counter: 0,
+    figure: null,
+    mode: 'Рисование'
+}
+
 function canvasOptions() {
     const canvas = document.getElementById('canvas')
     const ctx = canvas.getContext('2d')
@@ -24,14 +31,14 @@ function initRange(c) {
 
 
 function makeButton(btn) {
-    return `<button class="colors__button ${btn.class}" data-color="${btn.color}"></button>`
+    return `<button class="colors__button ${btn.class == 'black' ? btn.class + ' active' : btn.class}" data-color="${btn.color}"></button>`
 }
 
 
 function makeButtons(c) {
     const colors = document.getElementById('colors')
     colors.insertAdjacentHTML('afterbegin', buttons.map(btn => makeButton(btn)).join(' '))
-    
+
     const colorButtons = Array.from(colors.children)
 
     function changeColor(e) {
@@ -44,19 +51,17 @@ function makeButtons(c) {
     colorButtons.forEach(btn => btn.addEventListener('click', changeColor))
 }
 
-let filling = false
-
 function initMode() {
     const mode = document.getElementById('mode')
 
     mode.addEventListener('click', function () {
-        filling = !filling
-        
-        if (filling) {
-            mode.innerText = 'Заливка'
+
+        if (mode.innerText == 'Рисование') {
+            opt.mode = 'Заливка'
         } else {
-            mode.innerText = 'Рисование'
+            opt.mode = 'Рисование'
         }
+        mode.innerText = opt.mode
     })
 }
 
@@ -79,6 +84,57 @@ function initPicker(c) {
     })
 }
 
+
+
+function makeSquare(ctx, { x1, x2, y1, y2, figure }) {
+    figure == 'squareOutline' ? ctx.strokeRect(x1, y1, x2 - x1, y2 - y1) : ctx.fillRect(x1, y1, x2 - x1, y2 - y1)
+}
+
+function makeTriangle(ctx, { x1, x2, y1, y2, x3, y3, figure }) {
+    if (figure == 'triangleOutline') {
+        ctx.moveTo(x1, y1)
+        ctx.lineTo(x2, y2)
+        ctx.lineTo(x3, y3)
+        ctx.lineTo(x1, y1)
+        ctx.stroke()
+
+    } else {
+        ctx.moveTo(x1, y1)
+        ctx.lineTo(x2, y2)
+        ctx.lineTo(x3, y3)
+        ctx.fill()
+    }
+}
+
+function makeLine(ctx, { x1, x2, y1, y2 }) {
+    ctx.moveTo(x1, y1)
+    ctx.lineTo(x2, y2)
+    ctx.stroke()
+}
+
+function makeCircle(ctx, { x1, x2, y1, y2, figure }) {
+    let radX = x2 > x1 ? (x2 - x1) / 2 : (x1 - x2) / 2
+    let radY = y2 > y1 ? (y2 - y1) / 2 : (y1 - y2) / 2
+    let rad = radX
+
+    if (rad < 15) {
+        rad = radY
+    }
+
+    ctx.arc(x1 + ((x2 - x1) / 2), y1 + ((y2 - y1) / 2), rad, 0, getRadians(360))
+
+    if (figure == 'circle') {
+        ctx.fill()
+    } else {
+        ctx.stroke()
+    }
+
+    function getRadians(degrees) {
+        return (Math.PI / 180) * degrees;
+    }
+}
+
+
 export function initCanvas() {
     const { ctx, canvas } = canvasOptions()
 
@@ -86,11 +142,15 @@ export function initCanvas() {
     makeButtons(ctx)
 
     let painting = false
-    
 
     initMode()
     initSave(canvas)
     initPicker(ctx)
+
+    const fig = document.getElementById('figure')
+    fig.addEventListener('change', function (e) {
+        opt.figure = e.target.value
+    })
 
     canvas.addEventListener("mousemove", function (e) {
         if (!painting) {
@@ -103,7 +163,7 @@ export function initCanvas() {
     })
 
     canvas.addEventListener("mousedown", function (e) {
-        if (!filling) {
+        if (opt.mode == 'Рисование' && e.button == 0 && !opt.figure) {
             painting = true
         }
     })
@@ -116,9 +176,42 @@ export function initCanvas() {
         painting = false
     })
 
+    function toBase() {
+        opt.counter = 0
+        opt.figure = null
+        fig.value = 'base'
+    }
+
     canvas.addEventListener("click", function (e) {
-        if (filling) {
+        if (opt.mode == 'Заливка') {
             ctx.fillRect(0, 0, canvas.dataset.w, canvas.dataset.h)
+        }
+
+        if (opt.figure) {
+            console.log(opt);
+            opt.counter += 1
+            opt[`x${opt.counter}`] = e.offsetX
+            opt[`y${opt.counter}`] = e.offsetY
+
+            if (opt.counter == 2 && opt.figure.indexOf('triangle') < 0) {
+                if (opt.figure == 'line') {
+                    makeLine(ctx, opt)
+                }
+                if (opt.figure == 'square' || opt.figure == "squareOutline") {
+                    makeSquare(ctx, opt)
+                }
+                if (opt.figure == 'circle' || opt.figure == "circleOutline") {
+                    makeCircle(ctx, opt)
+                }
+
+                toBase()
+            }
+
+
+            if (opt.counter == 3 && opt.figure.indexOf('triangle') >= 0) {
+                makeTriangle(ctx, opt)
+                toBase()
+            }
         }
     })
 
